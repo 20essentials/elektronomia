98664723faae93bdd201fab49004482fc490e6f8
DominicDev <s0l0estudi4r@gmail.com>
2025-09-03 17:18:57 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'ELEKTRONOMIA';
+export const nameSongs = [
+	"SKY HIGH", /*1*/
+	"ENERGY", /*2*/
+	"LIMITLESS", /*3*/
+	"FREE", /*4*/
+	"SUMMERSONG", /*5*/
+	"SKY HIGH PT. II", /*6*/
+	"THE OTHER SIDE", /*7*/
+	"VITALITY", /*8*/
+	"MEMORY", /*9*/
+	"HEAVEN", /*10*/
+	"BREEZE", /*11*/
+	"COLLIDE", /*12*/
+	"ROLLERCOASTER", /*13*/
+	"CHAMPIONS", /*14*/
+	"CARAMEL", /*15*/
+	"NIGHTFALL", /*16*/
+	"RUMBLE", /*17*/
+	"HIGHLANDS", /*18*/
+	"BY OUR SIDE", /*19*/
+	"ROLLERCOASTER", /*20*/
+]
